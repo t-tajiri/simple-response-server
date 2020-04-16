8debf9499873cb7d20bc12d37d5f9973f56fcac7
t-tajiri <swallow.ava@gmail.com>
2020-04-16 17:51:44 +0900

    feat: handle connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
     let listner = TcpListener::bind("localhost:7878").unwrap();
+    // listner.set_nonblocking(true).expect("can not set non-blocking");
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        println!("hello");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request {}", String::from_utf8_lossy(&buffer[..]));
 }
