2c44b965a84c46cc7f96e72a64800b3b2bcbb6ed
t-tajiri <swallow.ava@gmail.com>
2020-04-17 23:46:01 +0900

    WIP: attach request header to response content

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::net::TcpListener;
 
 fn main() {
     let listner = TcpListener::bind("localhost:7878").unwrap();
-    // listner.set_nonblocking(true).expect("can not set non-blocking");
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
@@ -18,6 +17,17 @@ fn handle_connection(mut stream: TcpStream) {
 
 
     stream.read(&mut buffer).unwrap();
+    let response_info = String::from_utf8_lossy(&buffer[..]);
 
-    println!("Request {}", String::from_utf8_lossy(&buffer[..]));
+    stream.write("HTTP/1.1 200 OK\n".as_bytes()).unwrap();
+    stream.write("Content-Type: text/html; charset=UTF-8\n".as_bytes()).unwrap();
+    stream.write("Content-Length: 1000\n\n".as_bytes()).unwrap();
+    let ip = format!("{}", &stream.peer_addr().unwrap());
+    stream.write(ip.as_bytes()).unwrap();
+    stream.write("\n".as_bytes()).unwrap();
+    stream.write("\n".as_bytes()).unwrap();
+    stream.write(response_info.as_bytes()).unwrap();
+    stream.write("\n".as_bytes()).unwrap();
+    stream.write("\n".as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
