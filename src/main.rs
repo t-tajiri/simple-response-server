4d85c2e49f0c6455e940bd290621bad74ac46abf
t-tajiri <swallow.ava@gmail.com>
2020-04-20 17:34:00 +0900

    feat: add Connection header

--- src/main.rs
+++ src/main.rs
@@ -18,34 +18,37 @@ pub fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 4096];
 
     stream.read(&mut buffer).unwrap();
 
     send_response(&stream, &buffer);
 }
 
-fn send_response(mut stream: &TcpStream, buffer: &[u8; 1024]) {
-    let response_info = String::from_utf8_lossy(&buffer[..]);
+fn send_response(mut stream: &TcpStream, buffer: &[u8; 4096]) {
+    let response_info = String::from_utf8(buffer[..].to_vec()).unwrap();
+    let response_info = response_info.trim_matches(char::from(0));
+
     let ip = format!("{}", &stream.peer_addr().unwrap());
+
     let date = Utc::now()
         .with_timezone(&FixedOffset::east(9 * 3600))
         .format("%Y年%m月%d日 %H時%M分%S秒");
 
     let content = format!(
-        "<!doctype html>
-                           <html>
-                                <head></head>
-                                <body>
-                                    <h1>request info</h1>
-                                    <h2>remote access address</h2>
-                                    </div>{}</div>
-                                    <h2>request date</h2>
-                                    <div>{}</div>
-                                    <h2>header info</h2>
-                                    <div>{}</div>
-                                </body>
-                           </html>\n\n",
+        "<!DOCTYPE html>\r\n
+            <html>\r\n
+                <head></head>\r\n
+                <body>\r\n
+                    <h1>request info</h1>\r\n
+                    <h2>remote access address</h2>\r\n
+                    <div>{}</div>\r\n
+                    <h2>request date</h2>\r\n
+                    <div>{}</div>\r\n
+                    <h2>header info</h2>\r\n
+                    <div>{}</div>\r\n
+                </body>\r\n
+            </html>\r\n\r\n",
         ip, date, response_info
     );
 
@@ -57,10 +60,10 @@ fn send_response(mut stream: &TcpStream, buffer: &[u8; 1024]) {
 }
 
 fn create_response_header(mut stream: &TcpStream, content: &String) {
-    stream.write("HTTP/1.1 200 OK\n".as_bytes()).unwrap();
+    stream.write("HTTP/1.1 200 OK\r\n".as_bytes()).unwrap();
 
     stream
-        .write("Content-Type: text/html; charset=UTF-8;\n".as_bytes())
+        .write("Content-Type: text/html; charset=UTF-8;\r\n".as_bytes())
         .unwrap();
 
     stream.write("Content-Length: ".as_bytes()).unwrap();
@@ -68,8 +71,11 @@ fn create_response_header(mut stream: &TcpStream, content: &String) {
     // FIXME refactor converting usize to &[u8]
     let content_length = format!("{}", content.len());
     stream.write(content_length.as_bytes()).unwrap();
+    stream.write(";\r\n".as_bytes()).unwrap();
 
-    stream.write(";\n\n".as_bytes()).unwrap();
+    stream.write("Connection: close;\r\n".as_bytes()).unwrap();
+
+    stream.write("\r\n\r\n".as_bytes()).unwrap();
 }
 
 fn create_response_content(mut stream: &TcpStream, content: &String) {
